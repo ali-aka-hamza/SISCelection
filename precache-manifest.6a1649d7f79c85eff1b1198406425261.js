f2ccd83182aafff8258c50a6106497dcb05d01f0
ali-aka-hamza <alihamza.bscs19@iba-suk.edu.pk>
2022-07-28 13:09:25 +0500

    Updates

---
+++ precache-manifest.6a1649d7f79c85eff1b1198406425261.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "e75460a39f0b3673cab4",
+    "url": "/SISCelection/static/css/main.430a08b0.chunk.css"
+  },
+  {
+    "revision": "e75460a39f0b3673cab4",
+    "url": "/SISCelection/static/js/main.e75460a3.chunk.js"
+  },
+  {
+    "revision": "f2713309216dac7541b2",
+    "url": "/SISCelection/static/js/1.f2713309.chunk.js"
+  },
+  {
+    "revision": "e75841ec89e61cac3ff3",
+    "url": "/SISCelection/static/css/2.cad5a8de.chunk.css"
+  },
+  {
+    "revision": "e75841ec89e61cac3ff3",
+    "url": "/SISCelection/static/js/2.e75841ec.chunk.js"
+  },
+  {
+    "revision": "25d89a689ed67b60a5bb",
+    "url": "/SISCelection/static/js/runtime~main.25d89a68.js"
+  },
+  {
+    "revision": "77e34d8b9ed69fed05c5c76503802aa6",
+    "url": "/SISCelection/static/media/panel.77e34d8b.webp"
+  },
+  {
+    "revision": "8b25bad2c8f90f792dabe8ce7158e0e1",
+    "url": "/SISCelection/static/media/vote.8b25bad2.png"
+  },
+  {
+    "revision": "a821ed17388dee3fee16b9751accad54",
+    "url": "/SISCelection/index.html"
+  }
+];
